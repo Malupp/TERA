a64f52b2389624eb0075b78171c217d2dc254fab
Manuel Luppino <luppinomanuel@gmail.com>
2025-04-06 21:36:36 +0200

    removed steps, pages and classes will be used as a singleton

--- src/pages/googleSearch.py
+++ src/pages/googleSearch.py
@@ -1,6 +1,27 @@
 ## Example of a locator class, you must save here or in other pages like this your xpath and you can reuses in every part of the project
+from config import requestsUrl
+from src.functions.interactions import Interactions
 
 class GoogleSearch:
     searchButton = "//button[@aria-label='Cerca']"
     inputArea = "//textarea[@id='APjFqb']"
     rifiutaTutto = "//button[@id='W0wltc']"
+
+    def __init__(self, driver):
+        self.driver = driver  # Store the driver for reuse
+
+    @staticmethod
+    def navigateTo(site):
+        requestsUrl.getURL(f'{site}')
+
+    @staticmethod
+    def loginExample():
+        try:
+            GoogleSearch.navigateTo("www.google.it")
+            Interactions.click_element(GoogleSearch.rifiutaTutto)
+            Interactions.input_text(GoogleSearch.inputArea, "Demo")
+            Interactions.wait(3)
+            Interactions.press_enter()
+
+        except Exception as e:
+            print(f"ERRORE: {e}")
--- tests/examples/navigatingGoogle.py
+++ tests/examples/navigatingGoogle.py
@@ -1,7 +1,4 @@
-from src.steps.loginSteps import LoginSteps
-from config.driver import WebDriverManager
-
-driver = WebDriverManager.get_driver("chrome")
+from src.pages.googleSearch import GoogleSearch
 
 # This is a first test example, it will be replaced soon with pytest 
-LoginSteps.loginExample()
+GoogleSearch.loginExample()
